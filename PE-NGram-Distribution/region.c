b5557bf6b9e4a6b1bd1c8982d899aa3c79ab678d
ZSShen <andy780328@hotmail.com>
2014-06-10 08:00:19 -0700

    Prepare the internal and exported APIs of RegionCollector structure.

--- PE-NGram-Distribution/region.c
+++ PE-NGram-Distribution/region.c
@@ -1,11 +1,45 @@
 #include "region.h"
 
+/*===========================================================================*
+ *                  Definition for internal functions                        *
+ *===========================================================================*/
+
+/**
+ * This function selects the binary regions from the entire section with maximum average entropy.
+ * 
+ * @param   self        The pointer to the RegionCollector structure.
+ * @param   pPEInfo     The pointer to the to be analyzed PEInfo structure.
+ *
+ * @return              0: The binary regions are collected successfully.
+ *                    < 0: Exception occurs while memory allocation.
+ */
+int _FuncEntirePlanInMaxSec(RegionCollector *self, PEInfo *pPEInfo);
+
+
+/**
+ * This function selects the binary regions which have the entropy values higher than the threshold
+ * from the section with maximum average entropy.
+ *
+ * @param   self        The pointer to the RegionCollector structure.
+ * @param   pPEInfo     The pointer to the to be analyzed PEInfo structure.
+ *
+ * @return              0: The binary regions are collected successfully.
+ *                    < 0: Exception occurs while memory allocation.
+ */
+int _FuncPlateausInMaxSec(RegionCollector *self, PEInfo *pPEInfo);
+
+
+/*===========================================================================*
+ *                Implementation for exported functions                      *
+ *===========================================================================*/
 
 void RCInit(RegionCollector *self) {
 
+    /* Initialize member variables. */
     self->usNumRegions = 0;
     self->arrRegion = NULL;
 
+    /* Assign the default member functions. */
     self->selectFeatures = RCSelectFeatures;
 
     return;
@@ -43,6 +77,23 @@ void RCDeinit(RegionCollector *self) {
  * RCSelectFeatures() Select the features for n-gram model generation based on the specified method.
  */
 int RCSelectFeatures(RegionCollector *self, const char *cszMethod, PEInfo *pPEInfo) {
+    int rc;
+    
+    /* The default method is _FuncEntirePlanInMaxSec(). */
+    if (cszMethod == NULL)
+        rc = _FuncEntirePlanInMaxSec(self, pPEInfo);
+
+
+    return 0;
+}
+
+
+/*===========================================================================*
+ *                Implementation for internal functions                      *
+ *===========================================================================*/
+
+int _FuncEntirePlanInMaxSec(RegionCollector *self, PEInfo *pPEInfo) {
+
 
     return 0;
 }
