75cc3a3a84e004538b45813471e22515c2bb14ad
ZSShen <andy780328@hotmail.com>
2015-05-05 06:22:44 -0700

    Simplify the cleaning strategy of cleaner.

--- clean.py
+++ clean.py
@@ -20,37 +20,20 @@ def main():
     # Recursivly clean all the binary directories.
     list_clean = list();
     path_cur = os.getcwd();
-    for path_dir, list_dir, list_file in os.walk(path_cur):
-        if path_dir.endswith("build"):
-            list_clean.append(path_dir);
-        elif path_dir.endswith("lib"):
-            list_clean.append(path_dir);
-        elif path_dir.endswith("export"):
-            list_clean.append(path_dir);
-    for path_clean in list_clean:
-        if os.path.isdir(path_clean) == True:
-            shutil.rmtree(path_clean);
+    path_build = os.path.join(path_cur, "build");
+    if os.path.isdir(path_build) == True:
+        shutil.rmtree(path_build);
+    path_bin_plg = os.path.join(path_cur, "bin", "plugin");
+    if os.path.isdir(path_bin_plg) == True:
+        shutil.rmtree(path_bin_plg);
+    path_bin_eng = os.path.join(path_cur, "bin", "engine");
+    if os.path.isdir(path_bin_eng) == True:
+        shutil.rmtree(path_bin_eng);
 
     # Create the build folder if necessary.
-    if rebuild == False:
-        return;
-    for path_dir, list_dir, list_file in os.walk(path_cur):
-        if path_dir.endswith("src"):
-            path_build = path_dir.replace("src", "build");
-            if os.path.isdir(path_build) == False:
-                os.makedirs(path_build);
-        elif path_dir.endswith("plugin"):
-            path_build = os.path.join(path_dir, "build");
-            if os.path.isdir(path_build) == False:
-                os.makedirs(path_build);
-        elif path_dir.endswith("engine"):
-            path_build = os.path.join(path_dir, "build");
-            if os.path.isdir(path_build) == False:
-                os.makedirs(path_build);
-        elif path_dir.endswith("YaraGenerator"):
-            path_build = os.path.join(path_dir, "build");
-            if os.path.isdir(path_build) == False:
-                os.makedirs(path_build);
+    if rebuild == True:
+        if os.path.isdir(path_build) == False:
+            os.makedirs(path_build);
 
     return;
 
